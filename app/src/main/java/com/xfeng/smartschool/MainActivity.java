5cec7d29395bfec51cdb0af69cc73374a01ed6cc
xfeng_mac <xfengv@yeah.net>
2017-03-21 20:16:53 +0800

    更新了UI框架

--- app/src/main/java/com/xfeng/smartschool/MainActivity.java
+++ app/src/main/java/com/xfeng/smartschool/MainActivity.java
@@ -1,13 +1,46 @@
 package com.xfeng.smartschool;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle toggle;//代替监听器
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        initUI();
+        initToolbar();
     }
+    private void initUI() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    }
+
+    private void initToolbar() {
+
+
+        mToolbar.setTitle(R.string.app_name);
+        mToolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white));
+        mToolbar.setNavigationIcon(R.drawable.ic_menu);
+
+
+        //setTile 要在下面这句话上面，不然会失效
+        setSupportActionBar(mToolbar);
+
+        //监听DrawerLayout
+        //将抽屉事件和 toolbar联系起来，这是 material design 的设计
+        toggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(toggle);
+    }
+
+
 }
