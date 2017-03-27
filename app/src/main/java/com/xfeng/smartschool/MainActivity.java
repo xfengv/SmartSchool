037d5fbee964a5050d3ab83c86353897d3c2574f
xfeng_mac <xfengv@yeah.net>
2017-03-27 20:46:49 +0800

    更新了UI框架

--- app/src/main/java/com/xfeng/smartschool/MainActivity.java
+++ app/src/main/java/com/xfeng/smartschool/MainActivity.java
@@ -1,33 +1,75 @@
 package com.xfeng.smartschool;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.FrameLayout;
+import android.widget.RadioGroup;
+
+import com.xfeng.smartschool.fragment.NewsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "XFeng";
     private Toolbar mToolbar;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle toggle;//代替监听器
+    private RadioGroup mMainRadios;
+    private FragmentPagerAdapter mFragmentPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+        @Override
+        public Fragment getItem(int position) {
+            if (position == R.id.rb_tab_common) {
+                return NewsFragment.newInstance("FirstFragment");
+            } else if (position == R.id.rb_tab_sale) {
+                return NewsFragment.newInstance("FirstFragment");
+            } else {
+                return NewsFragment.newInstance("FirstFragment");
+            }
+        }
+        @Override
+        public int getCount() {
+            return 3;
+        }
+    };
+    private FrameLayout mFrameComtent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         initUI();
+        setListener();
         initToolbar();
     }
+
+    private void setListener() {
+        mMainRadios.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                Log.i(TAG,"选中 "+checkedId);
+                Fragment fragment =
+                        (Fragment) mFragmentPagerAdapter.instantiateItem(mFrameComtent, checkedId);
+                mFragmentPagerAdapter.setPrimaryItem(mFrameComtent, checkedId, fragment);
+                mFragmentPagerAdapter.finishUpdate(mFrameComtent);
+            }
+        });
+    }
+
     private void initUI() {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mMainRadios = (RadioGroup) findViewById(R.id.main_radios);
+        mFrameComtent = (FrameLayout) findViewById(R.id.frame_content);
     }
 
     private void initToolbar() {
-
-
         mToolbar.setTitle(R.string.app_name);
         mToolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white));
         mToolbar.setNavigationIcon(R.drawable.ic_menu);
@@ -41,6 +83,4 @@ public class MainActivity extends AppCompatActivity {
         toggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(toggle);
     }
-
-
 }
---
+++ app/src/main/java/com/xfeng/smartschool/fragment/NewsFragment.java
@@ -0,0 +1,44 @@
+package com.xfeng.smartschool.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.xfeng.smartschool.R;
+
+/**
+ * Created by xfeng
+ * data 17/3/27 20:27
+ * email xfengv@yeah.net
+ */
+
+public class NewsFragment extends Fragment{
+    private static final String PARAM = "param";
+    private String mParam;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.frag_first_container, null);
+        return view;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam = getArguments().getString(PARAM);
+        }
+    }
+
+
+    public static NewsFragment newInstance(String param) {
+        NewsFragment fragment = new NewsFragment();
+        Bundle args = new Bundle();
+        args.putString(PARAM, param);
+        fragment.setArguments(args);
+        return fragment;
+    }
+}
